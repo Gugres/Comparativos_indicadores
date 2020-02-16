6e4ed34ef67dce216649e48dad544f72156c16b0
gugres <gustavo.giro.resende@gmail.com>
2020-02-16 19:27:56 -0300

    Falta a limpeza e a coleta correta dos dados

--- comparativo_indicadores.py
+++ comparativo_indicadores.py
@@ -1,27 +1,26 @@
-# Notas:
-#  - Tentar criar uma função para converter os valores de string para inteiro
-
 import sys
 import pandas as pd
 
-def main():
-  df_B3 = pd.read_excel("dados/Setorial_B3_simplificado.xlsx")
+def PreparacaoIndicadores():
   df_indicadores = pd.read_excel("dados/fundamentus_ind.xlsx").T
-  df_relatorio = pd.read_excel("dados/relatorio_final_pupx.xlsx")
-  df_saida = pd.DataFrame()
-
   columns = df_indicadores.columns
   for x in columns:
     try:
       df_indicadores[x] = df_indicadores[x].map(lambda p: p.strip("%"))
-      df_indicadores[x] = df_indicadores[x].replace(to_replace=",", value=".", regex=True).astype(float)
-      print(x)
+      df_indicadores[x] = df_indicadores[x].apply(TiraPonto)
+      df_indicadores[x] = df_indicadores[x].replace(",",".",regex=True).astype(float)
     except:
+      print(x)
       continue
-  print(columns)
-  sys.exit()
+  df_indicadores.to_excel("dados/fundamentus_ind_ajustado.xlsx")
 
-  for i in range(1):
+def main():
+  df_B3 = pd.read_excel("dados/Setorial_B3_simplificado.xlsx")
+  df_indicadores = pd.read_excel("dados/fundamentus_ind_ajustado.xlsx")
+  df_relatorio = pd.read_excel("dados/relatorio_final_pupx.xlsx")
+  df_saida = pd.DataFrame()
+
+  for i in range(10):
     ativo = df_relatorio.alvo[i]
     ativo_B3 = ativo[:4]
     setor = df_B3.loc[df_B3.Ativo == ativo_B3].Setor
@@ -29,8 +28,17 @@ def main():
     ativos_setor = ativos_setor["Ativo"].tolist()
     ativos_fundamentus = BuscaAtivosPresentes(df_indicadores.index, ativos_setor)
 
-    df_ativo = df_indicadores.loc[ativo]
-    break
+    df_ativos_fundamentus = df_indicadores.loc[ativos_fundamentus]
+    serie_ativo = df_indicadores.loc[ativo]
+
+    serie_comparativo_media = df_ativos_fundamentus.mean()
+    serie_comparativo_variancia = df_ativos_fundamentus.std()
+
+    df_saida[ativo] = serie_ativo
+    df_saida["comparativo media: " + ativo] = serie_comparativo_media
+    df_saida["comparativo desvio padrao: " + ativo] = serie_comparativo_variancia
+
+  df_saida.to_excel("comparativos_indicadores.xlsx")
 
 def BuscaAtivosPresentes(ativos_fundamentus, ativos_setor):
   ativos_presentes = []
@@ -40,13 +48,18 @@ def BuscaAtivosPresentes(ativos_fundamentus, ativos_setor):
       ativos_presentes.append(x)
   return ativos_presentes
 
-# def TiraPorcentagem(x):
-#   columns = ["Cresc.5a","DY","EV/EBIT","EV/EBITDA","Liq.2m.","Liq.Corr.","Mrg.Liq.","P/Ativ.Circ.Liq.","P/Ativo","P/Cap.Giro","P/EBIT","P/L","P/VP","PSR","ROE","ROIC","cotacao"]
-#   # for x in columns:
-#     # df_indicadores[x] = df_indicadores[x].str.replace('%', '')
-#     # print(df_indicadores[x].head())
-#     # df_indicadores[x] = df_indicadores[x].astype(float)
-#   return x.strip("%")
+def TiraPonto(x):
+  x_novo = ""
+  for letra in x:
+    if letra != ".":
+      x_novo += letra
+  return x_novo
+
+def LimpezaDados(df_indicadores):
+  pass
 
 if __name__ == "__main__":
-  main()
+  ajuste = str(input("Deseja ajustar a tabela de indicadores? "))
+  if ajuste == "s":
+    PreparacaoIndicadores()
+  else: main()
